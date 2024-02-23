4297d3ed050fcefb8095360c8b2588b01ad1da2b
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-02-23 09:37:05 +0545

    .

--- LoginRoot/settings.py
+++ LoginRoot/settings.py
@@ -47,12 +47,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', #CORS Middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://localhost:8000/',
+]
+
 
 ROOT_URLCONF = 'LoginRoot.urls'
 
