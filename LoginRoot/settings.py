a48edc761e4886348f7c56ff9db6a817a64ebbe2
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-02-23 09:25:33 +0545

    .

--- LoginRoot/settings.py
+++ LoginRoot/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'Apis.apps.ApisConfig',
+    
+    'rest_framework', #RESTFramework
+    
+    'corsheaders', #CORS Header
 ]
 
 MIDDLEWARE = [
